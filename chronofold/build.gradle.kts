a99d78a04e3708862145f6f05f39bdf2aba3ae18
SerVB <servbul@yandex.ru>
2021-04-11 18:09:34 +0300

    Switch Chronofold module to multiplatform

--- chronofold/build.gradle.kts
+++ chronofold/build.gradle.kts
@@ -1,17 +1,28 @@
 plugins {
-    kotlin("js")
+    kotlin("multiplatform")
 }
 
 val kotestVersion: String by project
 val kotlinVersion: String by project
 
-dependencies {
-    implementation("io.kotest:kotest-assertions-core:$kotestVersion")
-    implementation(kotlin("test-js", kotlinVersion))
-}
-
 kotlin {
     js(IR) {
         browser()
+    }
+
+    sourceSets {
+        getByName("commonTest") {
+            dependencies {
+                implementation("io.kotest:kotest-assertions-core:$kotestVersion")
+                implementation(kotlin("test-common", kotlinVersion))
+                implementation(kotlin("test-annotations-common", kotlinVersion))
+            }
+        }
+
+        getByName("jsTest") {
+            dependencies {
+                implementation(kotlin("test-js", kotlinVersion))
+            }
+        }
     }
 }
